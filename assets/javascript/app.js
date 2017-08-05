f9ddf2f5bdbcd1c281e6173137a550e0e2ad38b9
Isaac Kwon <you@example.com>
2017-08-05 10:16:50 -0700

    6th Commit: Refreshing the page changes the value on HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-//Global Variables//
-var trainName = $("#train-name").val().trim();
-var trainDestination = $("#train-destination").val().trim();
-var trainTime = moment($("#train-time").val(), "HH:mm A").format("X");
-var trainFrequency = $("#train-frequency").val();
-
 //Initialize Firebase//
 var config = {
   apiKey: "AIzaSyDp3BE47XQTPv0RXq6xhi6TkTxsEXnuujE",
@@ -21,6 +15,14 @@ var database = firebase.database();
 $("#submit").on("click", function(event) {
   event.preventDefault();
 
+  var trainName = $("#train-name").val().trim();
+  var trainDestination = $("#train-destination").val().trim();
+  var trainTime = moment($("#train-time").val(), "HH:mm A").format("X");
+  var trainFrequency = $("#train-frequency").val();
+  var trainTimeFormat = moment.unix(trainTime).format("HH:mm");
+  console.log(trainTimeFormat);
+  console.log(trainTime);
+
   var train = {
     name: trainName,
     destination: trainDestination,
@@ -30,8 +32,6 @@ $("#submit").on("click", function(event) {
 
   //Push train object to the Firebase server//
   database.ref().push(train);
-
-  // alert("Train successfully added!");
 
   $("#train-name").val("");
   $("#train-destination").val("");
@@ -44,24 +44,15 @@ database.ref().on("child_added", function(childSnapshot) {
   var childObj = childSnapshot.val();
 
   var timeDiff = moment().diff(moment.unix(childObj.time), "minutes");
-  console.log(timeDiff);
-
   var trainTimeFormat = moment.unix(childObj.time).format("HH:mm");
-  console.log(trainTimeFormat);
-
   var trainFrequencyFormat = moment.unix(childObj.frequency).format("HH:mm");
-  console.log(trainFrequencyFormat);
-
-  console.log(trainFrequency);
 
   //============================================================================//
   //NEXT ARRIVAL & MINUTES AWAY CALCULATIONS//
 
-  // Assumptions
-  var tFrequency = 3;
+  var trainFrequency = $("#train-frequency").val();
 
-  // Time is 3:30 AM
-  var firstTime = "03:30";
+  var trainTime = moment($("#train-time").val(), "HH:mm A").format("X");
 
   // First Time (pushed back 1 year to make sure it comes before current time)
   var firstTimeConverted = moment(trainTimeFormat, "hh:mm").subtract(1, "years");
@@ -84,16 +75,16 @@ database.ref().on("child_added", function(childSnapshot) {
   console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
   // Next Train
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm A");
+  console.log("ARRIVAL TIME: " + nextTrain);
   //============================================================================//
 
   var newTr = $("<tr>");
 
   newTr.append("<td>" + childObj.name + "</td>");
   newTr.append("<td>" + childObj.destination + "</td>");
-  newTr.append("<td>" + trainFrequencyFormat + "</td>");
-  newTr.append("<td>" + nextTrain.format("HH:mm A") + "</td>");
+  newTr.append("<td>" + trainFrequency + "</td>");
+  newTr.append("<td>" + nextTrain + "</td>");
   newTr.append("<td>" + tMinutesTillTrain + "</td>");
 
   $("#table-body").append(newTr);
